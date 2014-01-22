03cc5921bb3d35628309de96b9cd92d938a99742
Aleksey Kulikov <AlekseyKulikov@hotmail.com>
2014-01-22 12:39:51 +0600

    Stub implementation

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Expose `gingkoImport()`.
+ */
+
+module.exports = gingkoImport;
+
+/**
+ * Convert markdown `text` to gingko tree json represenation.
+ *
+ * @param {String} text - markdown text
+ * @return {Object} - json represenation of gingko tree
+ * @api public
+ */
+
+function gingkoImport(text) {
+  // real implementation...
+}
